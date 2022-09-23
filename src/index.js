3ee7f2dba324e71bb205625da77219b7c084b80f
Aleksey <rusal@bk.ru>
2022-09-23 18:11:05 +0300

    добавил ежиков Артему

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( <
+    React.StrictMode >
+    <
+    App / >
+    Любите ли вы ёжиков так как люблю их я Артём не съел ёжик <
+    /React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
